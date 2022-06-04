327db29aca5a7126bc632b413fb6c6c332a4233b
Rebel <106883400+rebel-aleks@users.noreply.github.com>
2022-06-05 00:07:05 +0500

    Update com.js

--- js/com.js
+++ js/com.js
@@ -5,3 +5,17 @@ window.onload = function ( ) {
   alert ( 'test' ) ;
 
 };
+
+var i = 0 ;
+
+setInterval ( function ( ) {
+
+    document.title = ( i ) ? "HELLO!" : "I'm Aleks" ; 
+  
+    if ( i ) {
+      i = 0 ;
+    } else {
+      i = 1 ;
+    }
+
+} , 1000 ) ;
